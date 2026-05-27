aea9ad151c17c2fc0e3ef5724552c73ecf9fdd6c
인화 <iinhwa016@gmail.com>
2026-05-27 11:42:23 +0900

    주문 - xml 작성

--- src/main/java/com/school/schoolstock/domain/order/repository/OrderRepository.java
+++ src/main/java/com/school/schoolstock/domain/order/repository/OrderRepository.java
@@ -2,39 +2,38 @@ package com.school.schoolstock.domain.order.repository;
 
 import com.school.schoolstock.domain.order.vo.Orders;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface OrderRepository {
-    // 매도 주문 매칭
-    public Map<String, Object> getMatchOrder(int stockNo, int orderPrice, int orderAmount, String studentId, String content);
-
-    // 주문 요청 상태 '체결' 변경
-    public boolean setOrderStateMatched(int orderNo);
+    // 주문 매칭
+    Map<String, Object> getMatchOrder(int stockNo, int orderPrice, int orderAmount, String studentId, String content);
 
     // 매도, 매수 주문 요청
-    public boolean setOrderRequest(String content, int price, int amount, String state, String studentId, int stockNo);
+    boolean setOrderRequest(String content, int price, int amount, String state, String studentId, int stockNo);
 
     // 최근 등록한 주문 번호 조회
-    public int getMyOrderNo(String content, String studentId, int stockNo, String state, int amount, int price);
+    int getMyOrderNo(String content, String studentId, int stockNo, String state, int amount, int price);
 
     // 특정 주식 대기중인 매도 매수 주문 모두 조회
-    public List<Orders> getTotalOrder(int stockNo);
+    List<Orders> getTotalOrder(int stockNo);
 
     // 특정 주식 대기중인 매도 주문 모두 조회
-    public List<Orders> getTotalSellOrder(int stockNo);
+    List<Orders> getTotalSellOrder(int stockNo);
 
     // 특정 주식 대기중인 매수 주문 모두 조회
-    public List<Orders> getTotalBuyOrder(int stockNo);
+    List<Orders> getTotalBuyOrder(int stockNo);
 
     // 주문 요청 상태 '대기'변경
-    public boolean setOrderStatePending(int orderNo);
+    boolean setOrderStatePending(int orderNo);
 
     // 주문 요청 상태 '취소'변경
-    public boolean setOrderStateCancel(int orderNo);
+    boolean setOrderStateCancel(int orderNo);
+
+    // 주문 요청 상태 '체결' 변경
+    boolean setOrderStateMatched(int orderNo);
 
 
 }
--- src/main/java/com/school/schoolstock/domain/order/vo/Orders.java
+++ src/main/java/com/school/schoolstock/domain/order/vo/Orders.java
@@ -1,6 +1,21 @@
 package com.school.schoolstock.domain.order.vo;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Orders {
 
@@ -13,105 +28,4 @@ public class Orders {
     private String orderState;   // PENDING, MATCHED, CANCELED
     private LocalDateTime orderDate;
 
-    public Orders() {
-    }
-
-    public Orders(int orderNo, String studentId, int stockNo, String orderContent,
-                   int orderPoint, int amount, String orderState, LocalDateTime orderDate) {
-        this.orderNo = orderNo;
-        this.studentId = studentId;
-        this.stockNo = stockNo;
-        this.orderContent = orderContent;
-        this.orderPoint = orderPoint;
-        this.amount = amount;
-        this.orderState = orderState;
-        this.orderDate = orderDate;
-    }
-
-    public Orders(String studentId, int stockNo, String orderContent,
-                   int orderPoint, int amount) {
-        this.studentId = studentId;
-        this.stockNo = stockNo;
-        this.orderContent = orderContent;
-        this.orderPoint = orderPoint;
-        this.amount = amount;
-    }
-
-    public int getOrderNo() {
-        return orderNo;
-    }
-
-    public void setOrderNo(int orderNo) {
-        this.orderNo = orderNo;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public int getStockNo() {
-        return stockNo;
-    }
-
-    public void setStockNo(int stockNo) {
-        this.stockNo = stockNo;
-    }
-
-    public String getOrderContent() {
-        return orderContent;
-    }
-
-    public void setOrderContent(String orderContent) {
-        this.orderContent = orderContent;
-    }
-
-    public int getOrderPoint() {
-        return orderPoint;
-    }
-
-    public void setOrderPoint(int orderPoint) {
-        this.orderPoint = orderPoint;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getOrderState() {
-        return orderState;
-    }
-
-    public void setOrderState(String orderState) {
-        this.orderState = orderState;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Orders{" +
-                "orderNo=" + orderNo +
-                ", studentId='" + studentId + '\'' +
-                ", stockNo=" + stockNo +
-                ", orderContent='" + orderContent + '\'' +
-                ", orderPoint=" + orderPoint +
-                ", amount=" + amount +
-                ", orderState='" + orderState + '\'' +
-                ", orderDate=" + orderDate +
-                '}';
-    }
 }
