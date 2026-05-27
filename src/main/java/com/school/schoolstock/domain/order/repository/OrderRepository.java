afdc9cd3c549be80147e92ab1a4026cb763fc83e
인화 <iinhwa016@gmail.com>
2026-05-27 10:43:44 +0900

    주문 - 레퍼지토리 설정

--- src/main/java/com/school/schoolstock/domain/order/repository/OrderRepository.java
+++ src/main/java/com/school/schoolstock/domain/order/repository/OrderRepository.java
@@ -1,17 +1,40 @@
 package com.school.schoolstock.domain.order.repository;
 
+import com.school.schoolstock.domain.order.vo.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface OrderRepository {
+    // 매도 주문 매칭
+    public Map<String, Object> getMatchOrder(int stockNo, int orderPrice, int orderAmount, String studentId, String content);
 
-    Integer findLatestOrderNo(
-            @Param("orderContent") String orderContent,
-            @Param("studentId") String studentId,
-            @Param("stockNo") int stockNo,
-            @Param("orderState") String orderState,
-            @Param("amount") int amount,
-            @Param("orderPoint") int orderPoint
-    );
+    // 주문 요청 상태 '체결' 변경
+    public boolean setOrderStateMatched(int orderNo);
+
+    // 매도, 매수 주문 요청
+    public boolean setOrderRequest(String content, int price, int amount, String state, String studentId, int stockNo);
+
+    // 최근 등록한 주문 번호 조회
+    public int getMyOrderNo(String content, String studentId, int stockNo, String state, int amount, int price);
+
+    // 특정 주식 대기중인 매도 매수 주문 모두 조회
+    public List<Orders> getTotalOrder(int stockNo);
+
+    // 특정 주식 대기중인 매도 주문 모두 조회
+    public List<Orders> getTotalSellOrder(int stockNo);
+
+    // 특정 주식 대기중인 매수 주문 모두 조회
+    public List<Orders> getTotalBuyOrder(int stockNo);
+
+    // 주문 요청 상태 '대기'변경
+    public boolean setOrderStatePending(int orderNo);
+
+    // 주문 요청 상태 '취소'변경
+    public boolean setOrderStateCancel(int orderNo);
+
+
 }
