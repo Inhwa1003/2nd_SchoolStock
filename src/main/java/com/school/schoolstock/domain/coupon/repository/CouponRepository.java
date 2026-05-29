753aa5494bc4b20c93769b66b8d68686cd356678
이인화 <iinhwa016@gmail.com>
2026-05-30 00:39:22 +0900

    Coupon Repository Test 완료

--- src/main/java/com/school/schoolstock/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/school/schoolstock/domain/coupon/repository/CouponRepository.java
@@ -1,4 +1,14 @@
 package com.school.schoolstock.domain.coupon.repository;
 
-public class CouponRepository {
+import com.school.schoolstock.domain.coupon.vo.Coupons;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+
+public interface CouponRepository {
+
+    //쿠폰 상점에 등록된 쿠폰 조회
+    List<Coupons> getCouponList();
+
 }
