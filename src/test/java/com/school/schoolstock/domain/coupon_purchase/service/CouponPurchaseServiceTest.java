513edeb73b75a90e6322b14d64a1c955cc984faf
이인화 <iinhwa016@gmail.com>
2026-05-30 19:29:19 +0900

    Trade 서비스 테스까지 완료

--- src/test/java/com/school/schoolstock/domain/coupon_purchase/service/CouponPurchaseServiceTest.java
+++ src/test/java/com/school/schoolstock/domain/coupon_purchase/service/CouponPurchaseServiceTest.java
@@ -3,13 +3,12 @@ package com.school.schoolstock.domain.coupon_purchase.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 
-public class CouponServiceTest {
+public class CouponPurchaseServiceTest {
 
     @Autowired
     private CouponPurchaseService couponPurchaseService;
