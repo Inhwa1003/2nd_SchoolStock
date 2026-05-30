03fb04cff55a11667bfd3189dda2ea04bb70657d
이인화 <iinhwa016@gmail.com>
2026-05-30 17:06:25 +0900

    feat: 쿠폰 상점 service test 완료

--- src/test/java/com/school/schoolstock/domain/coupon_purchase/repository/CouponPurchaseRepositoryTest.java
+++ src/test/java/com/school/schoolstock/domain/coupon_purchase/repository/CouponPurchaseRepositoryTest.java
@@ -1,6 +1,5 @@
-package com.school.schoolstock.domain.coupon_purchase;
+package com.school.schoolstock.domain.coupon_purchase.repository;
 
-import com.school.schoolstock.domain.coupon_purchase.repository.CouponPurchaseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
