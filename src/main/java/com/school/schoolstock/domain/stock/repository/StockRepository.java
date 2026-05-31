f5e1dd3a4cc4350ccd46703fa87d2160a4c1d863
gitdong95 <choidongseok95@gmail.com>
2026-05-31 13:39:28 +0900

    StockRepository.java getStockName업무 추가

--- src/main/java/com/school/schoolstock/domain/stock/repository/StockRepository.java
+++ src/main/java/com/school/schoolstock/domain/stock/repository/StockRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface StockRepository {
+    // 보유 주식명 조회
+    String getStockName(int stockNo);
     // 주식명 리스트 조회
     List<StockVO> getStockNameList();
     // 주식 기본 정보 조회
