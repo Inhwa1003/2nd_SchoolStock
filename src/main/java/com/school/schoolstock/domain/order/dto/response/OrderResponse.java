ef373518d405d986a6ecca8892e4452004699a6f
인화 <iinhwa016@gmail.com>
2026-06-08 19:31:32 +0900

    feat: responseDTO 작성

--- src/main/java/com/school/schoolstock/domain/order/dto/response/OrderResponse.java
+++ src/main/java/com/school/schoolstock/domain/order/dto/response/OrderResponse.java
@@ -2,6 +2,8 @@ package com.school.schoolstock.domain.order.dto.response;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -9,10 +11,15 @@ import lombok.*;
 @Getter
 @Builder
 public class OrderResponse {
-    //종류 (BUY / SELL)
+    private int orderNo;
+    private String studentId;
+    private int stockNo;
+        //종류 (BUY / SELL)
     private String orderContent;
     //가격
     private int orderPoint;
     //수량
-    private int orderAmount;
+    private int amount;
+    private String orderState;
+    private LocalDateTime orderDate;
 }
