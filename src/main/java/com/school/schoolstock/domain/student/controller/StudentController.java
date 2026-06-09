08b33858e177cefe2aabdcae81c8b9901afa1394
gitdong95 <choidongseok95@gmail.com>
2026-06-09 11:37:57 +0900

    내 자산, 내 자산 새로고침(비동기) 내 포인트 내역, 내보유쿠폰 처리 완료

--- src/main/java/com/school/schoolstock/domain/student/controller/StudentController.java
+++ src/main/java/com/school/schoolstock/domain/student/controller/StudentController.java
@@ -1,4 +1,44 @@
 package com.school.schoolstock.domain.student.controller;
 
+import com.school.schoolstock.domain.student.dto.response.MyAssetResponse;
+import com.school.schoolstock.domain.student.service.StudentService;
+import com.school.schoolstock.global.security.SchoolUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequiredArgsConstructor
+@RequestMapping("/schoolstock/s/me/student")
+@Controller
 public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping("/history")
+    public String getMyPointHistory(@AuthenticationPrincipal SchoolUserDetails userDetails, Model model) {
+        model.addAttribute("pointHistory",studentService.getMyPointHistory(userDetails.getUsername()));
+        return "MyPointHistory";
+    }
+
+    @GetMapping("/assets")
+    public String studentMainView(@AuthenticationPrincipal SchoolUserDetails userDetails, Model model) {
+        model.addAttribute("myAssets", studentService.getMyAsset(userDetails.getUsername()));
+        return "MyAssets";
+    }
+
+    @GetMapping("/coupons")
+    public String getMyCoupons(@AuthenticationPrincipal SchoolUserDetails userDetails, Model model) {
+        model.addAttribute("studentName", userDetails.getUser().getLoginId());
+        model.addAttribute("coupons", studentService.getMyCoupon(userDetails.getUsername()));
+        return "MyCoupons";
+    }
+
+    @GetMapping("/assets/stocks")
+    @ResponseBody
+    public MyAssetResponse refreshMyAssets(@AuthenticationPrincipal SchoolUserDetails userDetails) {
+        return studentService.getMyAsset(userDetails.getUsername());
+    }
 }
