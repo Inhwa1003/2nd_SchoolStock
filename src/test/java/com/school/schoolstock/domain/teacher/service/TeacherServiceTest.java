9dbe26363ee46de4281a160fc3aeab4a87ad52f0
gitdong95 <choidongseok95@gmail.com>
2026-06-11 04:43:10 +0900

    service 테스트

--- src/test/java/com/school/schoolstock/domain/teacher/service/TeacherServiceTest.java
+++ src/test/java/com/school/schoolstock/domain/teacher/service/TeacherServiceTest.java
@@ -1,6 +1,6 @@
 package com.school.schoolstock.domain.teacher.service;
 
-import com.school.schoolstock.domain.teacher.dto.StudentListResponse;
+import com.school.schoolstock.domain.teacher.dto.response.StudentListResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -32,5 +32,21 @@ public class TeacherServiceTest {
         Assertions.assertFalse(result.isEmpty());
 
         log.info("담당 반 학생 목록 조회 결과 : {}", result);
+    }
+
+    @Test
+    public void getTeacherNameTest() {
+        String teacherId = "t0";
+
+        // NO
+        // 존재하지 않는 선생님 -> null
+        Assertions.assertNull(teacherService.getTeacherName("testTeacher"));
+
+        // YES
+        // 존재하는 선생님 이름
+        String name = teacherService.getTeacherName(teacherId);
+        Assertions.assertNotNull(name);
+        Assertions.assertNotEquals(teacherId, name);
+        log.info("선생님 이름 = {}", name);
     }
 }
