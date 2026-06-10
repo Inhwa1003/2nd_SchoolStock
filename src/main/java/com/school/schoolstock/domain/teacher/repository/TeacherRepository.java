62a8e24ad0c6b6cf553c58bdd950af67d93f5b7b
gitdong95 <choidongseok95@gmail.com>
2026-06-11 04:38:17 +0900

    getStudentIdInClass·getTeacherName·setPointGive

--- src/main/java/com/school/schoolstock/domain/teacher/repository/TeacherRepository.java
+++ src/main/java/com/school/schoolstock/domain/teacher/repository/TeacherRepository.java
@@ -1,7 +1,8 @@
 package com.school.schoolstock.domain.teacher.repository;
 
-import com.school.schoolstock.domain.teacher.dto.StudentListResponse;
+import com.school.schoolstock.domain.teacher.dto.response.StudentListResponse;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +14,13 @@ public interface TeacherRepository {
 
     // 선생님이 맡은 반 학생 목록 조회
     List<StudentListResponse> getMyStudents(String teacherId);
+
+    // 선생님이 맡은 반 학생 반번호로 아이디 조회
+    String getStudentIdInClass(@Param("teacherId") String teacherId, @Param("studentNumber") int studentNumber);
+
+    // 선생님 이름 조회(사이드바)
+    String getTeacherName(String teacherId);
+
+    // 포인트 지급
+    boolean setPointGive(String studentId, int point, String content);
 }
