5090f201cade199e0280a3e61f1e1554f48bd1af
gitdong95 <choidongseok95@gmail.com>
2026-06-11 04:38:42 +0900

    getStudentIdInClass·getTeacherName·setPointGive 위 3개 메서드 시그니처

--- src/main/java/com/school/schoolstock/domain/teacher/service/TeacherService.java
+++ src/main/java/com/school/schoolstock/domain/teacher/service/TeacherService.java
@@ -1,6 +1,6 @@
 package com.school.schoolstock.domain.teacher.service;
 
-import com.school.schoolstock.domain.teacher.dto.StudentListResponse;
+import com.school.schoolstock.domain.teacher.dto.response.StudentListResponse;
 
 import java.util.List;
 
@@ -8,4 +8,13 @@ public interface TeacherService {
 
     // 선생님이 맡은 반 학생 목록 조회
     List<StudentListResponse> getMyStudentsList(String teacherId);
+
+    // 선생님이 맡은 반 학생 반번호로 아이디 조회
+    String getStudentIdInClass(String teacherId, int studentNumber);
+
+    // 선생님 이름 조회(사이드바)
+    String getTeacherName(String teacherId);
+
+    // 포인트 지급
+    boolean givePoint(String studentId, int point, String content);
 }
