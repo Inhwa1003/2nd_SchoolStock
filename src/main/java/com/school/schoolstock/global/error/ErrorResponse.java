4b7d59ff3a41000c6384141f6a472a891ff8163f
gitdong95 <choidongseok95@gmail.com>
2026-06-12 04:27:51 +0900

    ErrorCode 및 ExceptionHandler 기반 마련

---
+++ src/main/java/com/school/schoolstock/global/error/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.school.schoolstock.global.error;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final int status;
+    private final String code;
+    private final String message;
+}
