ae638b238621dc8868aa22755f258ad946199d25
gitdong95 <choidongseok95@gmail.com>
2026-06-12 11:07:00 +0900

    TeacherRestController.java ApiResponse 형식으로 리턴 변경 여러 오류상황 service에 throw로 구현 ExceptionHandler가 받음 성공상황만 전달

--- src/main/java/com/school/schoolstock/domain/teacher/controller/TeacherRestController.java
+++ src/main/java/com/school/schoolstock/domain/teacher/controller/TeacherRestController.java
@@ -1,11 +1,13 @@
 package com.school.schoolstock.domain.teacher.controller;
 
+import com.school.schoolstock.domain.coupon.dto.CouponDeleteRequest;
+import com.school.schoolstock.domain.coupon.dto.CouponUpdateRequest;
 import com.school.schoolstock.domain.student.dto.response.MyAssetResponse;
 import com.school.schoolstock.domain.student.service.StudentService;
 import com.school.schoolstock.domain.teacher.dto.request.PointGrantRequest;
 import com.school.schoolstock.domain.teacher.service.TeacherService;
+import com.school.schoolstock.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -20,20 +22,37 @@ public class TeacherRestController {
     @GetMapping("/me/teachers/my-students/{studentNumber}/assets/stocks")
     public MyAssetResponse getMyStudentAssets(
             @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable int studentNumber
-    ) {
+            @PathVariable int studentNumber) {
 
         return  studentService.getMyAsset(teacherService.getStudentIdInClass(userDetails.getUsername(), studentNumber));
     }
 
     @PostMapping("/me/teachers/my-students/{studentNumber}/points")
-    public ResponseEntity<Void> grantPoints(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse grantPoints(@AuthenticationPrincipal UserDetails userDetails,
                                             @PathVariable int studentNumber,
                                             @RequestBody PointGrantRequest request) {
 
-        String studentId = teacherService.getStudentIdInClass(userDetails.getUsername(), studentNumber);
-        if (studentId == null) return ResponseEntity.status(403).build();
-        teacherService.givePoint(studentId, request.getPoints(), request.getContent());
-        return ResponseEntity.ok().build();
+        teacherService.givePoint(userDetails.getUsername(), studentNumber, request);
+        return ApiResponse.of(200, "포인트가 지급되었습니다.", null);
+    }
+    // 쿠폰 정보(쿠폰명, 쿠폰 포인트) 수정
+    @PostMapping("/coupons")
+    @ResponseBody
+    public ApiResponse setCoupon(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody CouponUpdateRequest request) {
+        teacherService.setCoupon(request);
+        return ApiResponse.of(200, "쿠폰 정보가 수정되었습니다.", null);
+    }
+
+    // 쿠폰 상점에 있는 쿠폰 삭제
+    @DeleteMapping("/coupons")
+    @ResponseBody
+    public ApiResponse deleteCoupon(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody CouponDeleteRequest request) {
+
+        teacherService.deleteCoupon(request.getCouponNo());
+        return ApiResponse.of(200, "쿠폰이 정상적으로 삭제되었습니다.", null);
     }
 }
