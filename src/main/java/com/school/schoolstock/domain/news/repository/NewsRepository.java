d833029e241eb852d1c7376d3bd60d2fd4e723a9
gitdong95 <choidongseok95@gmail.com>
2026-06-13 17:57:02 +0900

    getNewsManageList 선생님 뉴스 관리용 쿼리 및 메서드 Repository 정의

--- src/main/java/com/school/schoolstock/domain/news/repository/NewsRepository.java
+++ src/main/java/com/school/schoolstock/domain/news/repository/NewsRepository.java
@@ -1,10 +1,16 @@
 package com.school.schoolstock.domain.news.repository;
 
+import com.school.schoolstock.domain.news.vo.News;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface NewsRepository {
+    //뉴스조회
     List<String> getNewsList();
+
+    //선생님 관리용 목록(번호포함)
+    List<News> getNewsManageList();
+
 }
